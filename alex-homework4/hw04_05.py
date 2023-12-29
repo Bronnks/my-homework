64fb97265feb8440c6dcfb6f04102eb59b5bd9a4
alex <bonsidm@gmail.com>
2023-12-29 15:50:42 +0300

    Create hw04_05.py

---
+++ alex-homework4/hw04_05.py
@@ -0,0 +1,18 @@
+color = input()
+match color:
+    case '(255, 0, 0)':
+        print('Red')
+    case '(255, 165, 0)':
+        print('Orange')
+    case '(255, 255, 0)':
+        print('Yellow')
+    case '(0, 128, 0)':
+        print('Green')
+    case '(0, 0, 255)':
+        print('Blue')
+    case '(255, 255, 255)':
+        print('White')
+    case '(128, 128, 128)':
+        print('Gray')
+    case _:
+        print('Unknown color')
