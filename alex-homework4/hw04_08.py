6bdc99947660d983b21dfc3f6a8a547e45fa271f
alex <bonsidm@gmail.com>
2023-12-31 14:30:00 +0300

    Update hw04_08.py

--- alex-homework4/hw04_08.py
+++ alex-homework4/hw04_08.py
@@ -1,4 +1,6 @@
 chars = '!%@#$^&'
+
+
 def check_password(password):
     if len(password) < 8:
         return print('Пароль не надежный')
@@ -6,7 +8,6 @@ def check_password(password):
         if i not in chars and not i.isalnum():
             return print('Пароль не надежный')
     print('Пароль надежный')
-
 
 
 x = input()
