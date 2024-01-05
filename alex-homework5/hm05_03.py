02251537ee9c4320c55a4b63a20ea97cf16f94a1
alex <bonsidm@gmail.com>
2024-01-05 19:59:55 +0300

    Update hm05_03.py

--- alex-homework5/hm05_03.py
+++ alex-homework5/hm05_03.py
@@ -5,6 +5,7 @@ for i in range(len(lst)):
     res = lst[i].split()
     dct[i+1] = len(res)
 
+
 for key, value in dct.items():
     # print(f'Sentence {key} has {value} words.', end = ' ')
     print('Sentence {} has {} words.'.format(key, value), end=' ')
