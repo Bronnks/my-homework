3e6944e00d44ad92ba1a57c7e27e675a609feaba
alex <bonsidm@gmail.com>
2024-01-05 20:49:08 +0300

    Delete hm05_01.py

--- alex-homework5/hm05_01.py
+++
@@ -1,4 +0,0 @@
-number = input()
-print(f'+375 ({number[2:4]}) {number[4:7]}-{number[7:9]}-{number[9:]}')
-print('+375 (%s) %s-%s-%s' % (number[2:4], number[4:7], number[7:9], number[9:]))
-print('+375 ({}) {}-{}-{}'.format(number[2:4], number[4:7], number[7:9], number[9:]))
