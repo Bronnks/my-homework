dce8da4c0e634b025305411162b743a01804c33b
alex <bonsidm@gmail.com>
2024-01-05 20:49:05 +0300

    Delete hm05_03.py

--- alex-homework5/hm05_03.py
+++
@@ -1,8 +0,0 @@
-lst = input().split('. ')
-res1, res2, res3 = '', '', ''
-for i in range(len(lst)):
-    res1 += 'Sentence {} has {} words. '.format(i + 1, len(lst[i].split()))
-    res2 += f'Sentence {i + 1} has {len(lst[i].split())} words. '
-    res3 += 'Sentence %s has %s words. ' % (i + 1, len(lst[i].split()))
-
-print(res1, res2, res3, sep='\n')
