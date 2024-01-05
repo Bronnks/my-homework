7c1edc188cee06cb2b8008b1e735675defac5744
alex <bonsidm@gmail.com>
2024-01-05 20:25:45 +0300

    Update hm05_03.py

--- alex-homework5/hm05_03.py
+++ alex-homework5/hm05_03.py
@@ -1,6 +1,8 @@
 lst = input().split('. ')
-
+res1, res2, res3 = '', '', ''
 for i in range(len(lst)):
-    # print('Sentence {} has {} words.'.format(i+1, len(lst[i].split())), end=' ')
-    # print(f'Sentence {i+1} has {len(lst[i].split())} words.', end = ' ')
-    print('Sentence %s has %s words.' % (i + 1, len(lst[i].split())), end=' ')
+    res1 += 'Sentence {} has {} words. '.format(i + 1, len(lst[i].split()))
+    res2 += f'Sentence {i + 1} has {len(lst[i].split())} words. '
+    res3 += 'Sentence %s has %s words. ' % (i + 1, len(lst[i].split()))
+
+print(res1, res2, res3, sep='\n')
