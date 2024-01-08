f9c41481ad5fef61f82814ed65c84787a8fbc926
alex <bonsidm@gmail.com>
2024-01-08 11:39:29 +0300

    Update hw04_10.py

--- alex-homework4/hw04_10.py
+++ alex-homework4/hw04_10.py
@@ -2,16 +2,13 @@ chars = '!%@#$^&'
 
 
 def check_password(password):
-    count = 0
     if len(password) < 8 or ' ' in password:
         return print('Пароль не надежный')
-    if password.isalnum or any(i in chars for i in password):
-        count+=1
-    if count < len(password):
-        print('Пароль надежный')
-
-    else:
+    if password.isdigit() or password.isalpha() and password.isupper() or password.isalpha() and password.islower() or all(
+            i in chars for i in password):
         print('Пароль не надежный')
+    else:
+        print('Пароль надежный')
 
 
 x = input()
