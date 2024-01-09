0e0c6b71bf0fe82cc23bada6b66a6db93962f563
alex <bonsidm@gmail.com>
2024-01-09 18:26:56 +0300

    Update hw05_05.py

--- alex-homework5/hw05_05.py
+++ alex-homework5/hw05_05.py
@@ -1,4 +1,4 @@
-binary = int('101010', 2)
+binary = int(input(), 2)
 print(f'Decimal: {binary}, Octal: {binary:o}, Hexadecimal: {binary:X}.')
 print('Decimal: %d, Octal: %o, Hexadecimal: %X.' % (binary, binary, binary))
 print('Decimal: {0:d}, Octal: {0:o}, Hexadecimal: {0:X}.'.format(binary))
