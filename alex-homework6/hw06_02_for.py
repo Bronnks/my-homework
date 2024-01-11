51e6e256674bd3fa9d6c1b985cdbed800245025f
alex <bonsidm@gmail.com>
2024-01-11 20:03:27 +0300

    Update hw06_02_for.py

--- alex-homework6/hw06_02_for.py
+++ alex-homework6/hw06_02_for.py
@@ -1,6 +1,14 @@
-lst = [[i for i in range(1,11)] for j in range(1, 11)]
-for row in lst[1:]:
-    for cow in row:
-        lst[i][j] = i*j
-for row in lst:
-    print(*row)
+lst = [[i for i in range(1, 11)] for j in range(10)]
+
+for i in range(len(lst)):
+    lst[i][0] = lst[0][i]
+
+for i in range(1, len(lst)):
+    for j in range(i, len(lst)):
+        lst[i][j] = (i + 1) * (j + 1)
+        lst[j][i] = (i + 1) * (j + 1)
+
+for i in range(len(lst)):
+    for j in range(len(lst)):
+        print(str(lst[i][j]).center(4), end=' ')
+    print()
