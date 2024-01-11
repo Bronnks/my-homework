486322ff657d696495c2d8e155a7a4ec5d66bad6
alex <bonsidm@gmail.com>
2024-01-11 20:03:15 +0300

    Create hw06_05.py

---
+++ alex-homework6/hw06_05.py
@@ -0,0 +1,17 @@
+import copy
+
+
+def transpor_massive(lst):
+    lst2 = copy.deepcopy(lst)
+    for i in range(len(lst)):
+        for j in range(len(lst[0])):
+            lst2[i][j] = lst[j][i]
+    for row in lst2:
+        print(*row)
+
+
+n = int(input('введите кол-во вложенных списков - '))
+test = []
+for _ in range(n):
+    test.append([i for i in input('введите список чисел через запятую - ').split(',')])
+transpor_massive(test)
