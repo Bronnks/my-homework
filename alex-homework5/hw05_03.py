5f0aa3e4ab008cb3daba4df5a8daa2bef9a1d45a
alex <bonsidm@gmail.com>
2024-01-11 11:33:36 +0300

    Update hw05_03.py

--- alex-homework5/hw05_03.py
+++ alex-homework5/hw05_03.py
@@ -1,8 +1,17 @@
-lst = input().rstrip('.').split('.')
-res1, res2, res3 = '', '', ''
-for i in range(len(lst)):
-    res1 += 'Sentence {} has {} words. '.format(i + 1, len(lst[i].split()))
-    res2 += f'Sentence {i + 1} has {len(lst[i].split())} words. '
-    res3 += 'Sentence %s has %s words. ' % (i + 1, len(lst[i].split()))
+def func(text):
+    i = 1
+    for sentence in text.split('.'):
+        words = sentence.strip().split()
+        if not words:
+            continue
+        res1 = 'Sentence {} has {} words. '.format(i, len(words))
+        # res2 = f'Sentence {i} has {len(words)} words. '
+        # res3 = 'Sentence %s has %s words. ' % (i, len(words))
+        print(res1, end = ' ')
+        i += 1
+        # print(res2)
+        # print(res3)
 
-print(res1, res2, res3, sep='\n')
+
+lst = input()
+func(lst)
