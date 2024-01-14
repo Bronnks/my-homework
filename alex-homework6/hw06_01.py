2188cd70f6243b0fd189fcf6e9b2dc77ec82b1f2
alex <bonsidm@gmail.com>
2024-01-14 13:08:36 +0300

    Update hw06_01.py

--- alex-homework6/hw06_01.py
+++ alex-homework6/hw06_01.py
@@ -8,16 +8,16 @@ def nod(num1, num2):
             return abs(a)
         case _:
             divisors1 = [1, abs(num1)]  # делителем любого числа является 1 и само это число
-            for i in range(2, abs(num1) // 2 + 1):
+            for i in range(2, abs(num1) // 2 + 1):  # ищем делители первого числа(до середины), дальше они повторяются
                 if num1 % i == 0:
                     divisors1.append(i)
 
-            divisors2 = [1, abs(num2)]
+            divisors2 = [1, abs(num2)]  # ищем делители второго
             for i in range(2, abs(num2) // 2 + 1):
                 if num2 % i == 0:
                     divisors2.append(i)
 
-            common_divisors = []
+            common_divisors = []        # ищем общие делители
             for i in divisors1:
                 if i in divisors2:
                     common_divisors.append(i)
