11cb6eb647b4294900224c54870edf9469c23516
alex <bonsidm@gmail.com>
2024-01-14 13:12:58 +0300

    Update hw06_02_for.py

--- alex-homework6/hw06_02_for.py
+++ alex-homework6/hw06_02_for.py
@@ -1,14 +1,14 @@
-lst = [[i for i in range(1, 11)] for j in range(10)]
+lst = [[i for i in range(1, 11)] for j in range(10)]  # создаем матрицу 10х10 с уже заполненной верхней строкой
 
-for i in range(len(lst)):
+for i in range(len(lst)):  # заполняем первый столбец
     lst[i][0] = lst[0][i]
 
-for i in range(1, len(lst)):
+for i in range(1, len(lst)):  # заполняем остальные ячейки
     for j in range(i, len(lst)):
         lst[i][j] = (i + 1) * (j + 1)
         lst[j][i] = (i + 1) * (j + 1)
 
-for i in range(len(lst)):
+for i in range(len(lst)):  # красиво выводим таблицу умноржени
     for j in range(len(lst)):
         print(str(lst[i][j]).center(4), end=' ')
     print()
