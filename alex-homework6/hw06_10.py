6f400b60c6261bc82f7cdadc8f3a40aecf1a6962
alex <bonsidm@gmail.com>
2024-01-14 13:04:11 +0300

    Create hw06_10.py

---
+++ alex-homework6/hw06_10.py
@@ -0,0 +1,13 @@
+def check_brackets(line):
+    if len(line) % 2 == 1:
+        print('Строка не допустимая')
+    while any(('()' in line, '[]' in line, '{}' in line)):
+        line = line.replace('()', '').replace('[]', '').replace('{}', '')
+    if len(line) > 0:
+        print('Строка не допустимая')
+    else:
+        print('Строка допустимая')
+
+
+test = input('Введите строку со скобками - ')
+check_brackets(test)
