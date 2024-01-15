2f6da5c54beb3ff1dc617d2e8157379172c1be3f
alex <bonsidm@gmail.com>
2024-01-15 11:31:09 +0300

    Create hw07_01.py

---
+++ alex-homework7/hw07_01.py
@@ -0,0 +1,20 @@
+def minmax(lst):
+    min_num = lst[0]
+    max_num = lst[0]
+    for i in lst:
+        if i < min_num:
+            min_num = i
+        elif i > max_num:
+            max_num = i
+    res = (int_or_float(min_num), int_or_float(max_num))
+    print(res)
+
+
+def int_or_float(n):  # проверяем число целое или не очень, для более красивого вывода
+    if n - int(n) == 0:
+        n = int(n)
+    return n
+
+
+test = [float(i) for i in input('Введите список чисел через запятую - ').split(',')]
+minmax(test)
