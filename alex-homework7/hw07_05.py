cb80eb25c0b498e080c418a1f67445e02040bece
alex <bonsidm@gmail.com>
2024-01-18 12:02:54 +0300

    Update hw07_05.py

--- alex-homework7/hw07_05.py
+++ alex-homework7/hw07_05.py
@@ -1,9 +1,9 @@
 def find_ind_max(tpl):
     max_index = 0
     max_num = tpl[0]
-    for i in range(len(tpl)):
-        if tpl[i] > max_num:
-            max_num, max_index = tpl[i], i
+    for i, el in enumerate(tpl):
+        if el > max_num:
+            max_num, max_index = el, i
     print(f'Максимальное число {max_num} имеет индекс - {max_index}')
 
 
