17753cf72d52992025b15fa7ec766ef9d072b4a9
alex <bonsidm@gmail.com>
2024-02-02 11:03:45 +0300

    Update hw11_01.py

--- alex-homework11/hw11_01.py
+++ alex-homework11/hw11_01.py
@@ -1,38 +1,34 @@
 class Task:
-    task_list = []
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self):
+        self.task_list = []
 
-    def add_task(self):
+    def add_task(self, value):
         try:
-            if len(Task.task_list) > 19:
+            if len(self.task_list) > 19:
                 raise Exception
             else:
-                Task.task_list.append(self.name)
+                self.task_list.append(value)
         except Exception:
             print('Список задач переполнен.')
 
-    def remove_task(self):
-        if not Task.task_list:
+    def remove_task(self, value):
+        if not self.task_list:
             print('Список пуст, добавьте задачу сначала.')
             return
         try:
-            Task.task_list.remove(self.name)
+            self.task_list.remove(value)
         except ValueError:
-            print(f'Задачи {self.name} нет в списке.')
+            print(f'Задачи {value} нет в списке.')
 
-    @staticmethod
-    def check_empty_list():
-        if not Task.task_list:
+    def check_empty_list(self):
+        if not self.task_list:
             print('Список пуст.')
         else:
             print('В списке задач есть хотя бы одна задача.')
 
-    @staticmethod
-    def clear_list():
-        Task.task_list.clear()
+    def clear_list(self):
+        self.task_list.clear()
 
-    @staticmethod
-    def len_list():
-        print(f'Длина списка задач: {len(Task.task_list)}')
+    def len_list(self):
+        print(f'Длина списка задач: {len(self.task_list)}')
